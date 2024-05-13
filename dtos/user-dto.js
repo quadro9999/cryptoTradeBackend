2ffc6fe39721d8f00beb35303e77800fcb93346e
quadro9999 <138797260+quadro9999@users.noreply.github.com>
2024-05-13 21:23:23 +0300

    Update user-dto.js

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -2,11 +2,13 @@ module.exports = class UserDto {
   email;
   id;
   isActivated;
+  veridication;
 
   constructor(model) {
     this.email = model.email;
     this.id = model._id;
     this.isActivated = model.isActivated;
     this.isAdmin = model.isAdmin;
+    this.veridication = model.verification;
   }
 };
