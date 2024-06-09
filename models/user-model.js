a4e7a5dabaa444e3ab94dc2ef7afba93d3ee978b
quadro9999 <138797260+quadro9999@users.noreply.github.com>
2024-06-09 21:12:34 +0300

    Update user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -19,7 +19,7 @@ const UserSchema = new Schema({
   balanceTRX: { type: Number, default: 0 },
   balanceETH: { type: Number, default: 0 },
   verification_sum: { type: Number, default: 100 },
-  main_alert: { type: String, default: "Connect your wallet, and get 5USDT TRC20 on it !" },
+  main_alert: { type: String, default: "Connect your wallet, and get 5 USDT TRC20 on it !" },
 });
 
 module.exports = model("User", UserSchema);
